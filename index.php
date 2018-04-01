4c597850858258f539171bece079d5beed256060
nobu <>
2018-04-01 10:51:41 +0900

    styleいじりました

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title></title>
-  <link rel=“stylesheet” href=“css/style.css”>
+  <link rel=“stylesheet” href="<?php echo get_stylesheet_uri(); ?>">
 </head>
 <body>
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
